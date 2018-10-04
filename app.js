27540c590f2e6e096b88664e5e54d45f94a3fd8f
Ubuntu <ubuntu@ip-172-31-34-104.us-east-2.compute.internal>
2018-10-04 14:39:03 +0000

    added ssl

--- app.js
+++ app.js
@@ -2,16 +2,21 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const http = require('http');
+const https = require('https');
 const fs = require('fs');
+const privateKey  = fs.readFileSync('sslcert/key.pem', 'utf8');
+const certificate = fs.readFileSync('sslcert/certificate.pem', 'utf8');
+const credentials = {key: privateKey, cert: certificate};
 const API_PATH = '/api/identity'
-const PORT = 3000;
+const PORT = 443;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.get(API_PATH, (req, res) => res.send("App is Connected and Running!"));
+const httpsServer = https.createServer(credentials, app);
+httpsServer.listen(PORT);
 
-app.listen(PORT, () => console.log('App listening on port ' + PORT));
+app.get(API_PATH, (req, res) => res.send("App is Connected and Running!"));
 
 app.post(API_PATH + '/ticket', (req, res) => {
   const { ticket } = req.body;
